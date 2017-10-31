7d536d50f5fa947cbb86a747f40f0e1f3180dd2c
Brian Akers <brianakers@Brians-MacBook-Pro.local>
2017-10-31 16:38:14 -0400

    added a feature to check or uncheck paid bills

--- app/controllers/api/v1/bills_controller.rb
+++ app/controllers/api/v1/bills_controller.rb
@@ -2,31 +2,28 @@ require 'date'
 require 'pry'
 
 class Api::V1::BillsController < ApiController
-  skip_before_action :verify_authenticity_token, only: [:create]
+  skip_before_action :verify_authenticity_token, only: [:create, :update]
   def index
 
     bills= Bill.where(user_id: current_user)
     current_date = Date.current
+    all_bills=[]
+    all_bills_this_month= []
     unpaid_this_month= []
-    all_this_month= []
-    unpaid_all =[]
 
     bills.each do |bill|
       if !bill.paid && current_date.month == bill.due_date.month
+        all_bills_this_month << bill
         unpaid_this_month << bill
-        unpaid_all << bill
-        all_this_month << bill
       elsif bill.paid && current_date.month == bill.due_date.month
-        all_this_month << bill
-      else !bill.paid
-        unpaid_all << bill
+        all_bills_this_month << bill
       end
     end
 
     bills = {
-      unpaid_this_month: unpaid_this_month,
-      all_this_month: all_this_month,
-      unpaid_all: unpaid_all
+      all_bills: bills,
+      all_bills_this_month: all_bills_this_month,
+      unpaid_this_month: unpaid_this_month
     }
     render json: bills
   end
@@ -35,6 +32,37 @@ class Api::V1::BillsController < ApiController
     bill= JSON.parse(request.body.read)
     new_bill=Bill.create(name: bill["name"], cost: bill["cost"].to_f, source: bill["source"], due_date: bill["date"].to_date, user_id: current_user.id)
     render json: new_bill
+  end
+
+  def update
+    bill= Bill.find(params[:id])
+    if bill.paid
+      bill.update(paid: false)
+    elsif !bill.paid
+      bill.update(paid: true)
+    end
+
+    bills= Bill.where(user_id: current_user)
+    current_date = Date.current
+    all_bills=[]
+    all_bills_this_month= []
+    unpaid_this_month= []
+    
+    bills.each do |bill|
+      if !bill.paid && current_date.month == bill.due_date.month
+        all_bills_this_month << bill
+        unpaid_this_month << bill
+      elsif bill.paid && current_date.month == bill.due_date.month
+        all_bills_this_month << bill
+      end
+    end
+
+    bills = {
+      all_bills: bills,
+      all_bills_this_month: all_bills_this_month,
+      unpaid_this_month: unpaid_this_month
+    }
+    render json: bills
   end
 
 end
--- react/src/components/BillsTable.js
+++ react/src/components/BillsTable.js
@@ -3,22 +3,44 @@ import React from 'react';
 const BillsTable= (props) => {
   let paid
   if (props.bill.paid) {
-    paid = "yes"
+    paid= true
   } else {
-    paid = "no"
+    paid= false
   }
+
+  let handleCheckBoxClick= ()=> {
+    if (paid){
+      if (confirm("Are you sure you want to mark this bill as UNPAID?")){
+        let billPayLoad= {
+          id: props.bill.id
+        }
+        props.changeBillPaidState(billPayLoad)
+      }
+    } else{
+      if (confirm("Are you sure you want to mark this bill as PAID?")){
+        let billPayLoad= {
+          id: props.bill.id
+        }
+        props.changeBillPaidState(billPayLoad)
+      }
+    }
+  }
+
   return(
     <tr>
       <td> {props.bill.name} </td>
       <td> {props.bill.due_date} </td>
       <td> {props.bill.cost} </td>
       <td> {props.bill.source} </td>
-      <td> {paid} </td>
+      <td>
+        <form className="custom">
+          <label>
+            <input type="checkbox" id="checkbox1" onChange={handleCheckBoxClick} checked={paid}/>
+          </label>
+        </form>
+      </td>
     </tr>
   )
 }
-
-
-
 
 export default BillsTable;
