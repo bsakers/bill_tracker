31c08df6685d236c03b7a5cd65ec6a506492968d
Brian Akers <brianakers@Brians-MacBook-Pro.local>
2017-11-17 13:46:41 -0500

    moved oauth login/secret to env

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,7 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :google_oauth2, Rails.application.secrets.OAUTH_LOGIN, Rails.application.secrets.OAUTH_SECRET, skip_jwt: true
   # provider :google_oauth2, ENV["OAUTH_LOGIN"], ENV["OAUTH_SECRET"], skip_jwt: true
-  provider :google_oauth2, '1002781387609-jktlp53bku8gcffp83ejdv1a40vqscea.apps.googleusercontent.com', 'I-DZDlBBLH9sr02AJKxl_img', skip_jwt: true
+  # provider :google_oauth2, '1002781387609-jktlp53bku8gcffp83ejdv1a40vqscea.apps.googleusercontent.com', 'I-DZDlBBLH9sr02AJKxl_img', skip_jwt: true
 end
